734b4035e30ee4b0e5d40351eb86ddffa7d11b66
Megagig <megagigsoftwaresolutions.com>
2024-09-02 14:34:46 +0100

    Import Apexcharts

--- src/components/admin/AreaChart.jsx
+++ src/components/admin/AreaChart.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+import { MdArrowDropDown } from 'react-icons/md';
 
 const AreaChart = () => {
-  return (
-    <div>AreaChart</div>
-  )
-}
+  return <div>AreaChart</div>;
+};
 
-export default AreaChart
+export default AreaChart;
