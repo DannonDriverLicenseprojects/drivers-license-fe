e0b8eb1afa54f3502cdc44ec4b47febaa5873dd6
IYKE <ikegwimik@gmail.com>
2024-09-02 09:46:00 +0100

    created metric card and layout

--- src/pages/admin/new-applicants/NewApplicants.jsx
+++ src/pages/admin/new-applicants/NewApplicants.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import ApplicationBody from '../../../components/layouts/admin/body/ApplicationBody'
+import { useEffect } from "react"
+import { useOutletContext } from "react-router-dom"
+import MetricsCard from '../../../components/admin/MetricsCard'
+
 
 const NewApplicants = () => {
+
+  const { setPageName } = useOutletContext()
+
+  useEffect(() => {
+    setPageName('New Applicant')
+  }, [])
+
   return (
-    <div>NewApplicants</div>
+    <ApplicationBody
+      metricsCard={
+        <MetricsCard
+          title={"Total New Applicant"}
+          type={"new"}
+          currentTotal={40689}
+          prevTotal={40000}
+
+        />
+      }
+      table={"table should be here"}
+      chart={"chart should be here"}
+    />
   )
 }
 
