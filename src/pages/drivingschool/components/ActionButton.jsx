2bf2f8d16bb70e9e0761581de0690033b3b8ad55
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-10-01 17:25:26 +0100

    feat: Reuse driving school page for capture center

--- src/pages/drivingschool/components/ActionButton.jsx
+++ src/pages/drivingschool/components/ActionButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ActionButton = ({ children, icon, styles, ...rest }) => {
     return (
         <button
-            className={`${styles} w-full font-medium py-5 flex items-center justify-center gap-2 rounded-xl`}
+            className={`${styles} w-full font-medium py-4 flex items-center justify-center gap-2 rounded-xl`}
             {...rest}
         >
             {icon}
